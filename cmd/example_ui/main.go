a5c77789704f6879f22401a822680d17ae977430
Seokmin <bsmmoon@gmail.com>
2026-01-09 04:06:03 +0000

    [#7] chore: rename context to game context. split into its own file. move imageSrcRects into the new file

--- cmd/example_ui/main.go
+++ cmd/example_ui/main.go
@@ -15,17 +15,12 @@
 package main
 
 import (
-	"bytes"
 	"image"
 	"image/color"
-	_ "image/png"
 	"log"
 	"strings"
 
-	"golang.org/x/image/font/gofont/goregular"
-
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/examples/resources/images"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/text/v2"
 )
@@ -35,53 +30,6 @@ const (
 	lineSpacingInPixels = 16
 )
 
-type Context struct {
-	uiImage      *ebiten.Image
-	uiFaceSource *text.GoTextFaceSource
-}
-
-func NewContext() *Context {
-	img, _, err := image.Decode(bytes.NewReader(images.UI_png))
-	if err != nil {
-		log.Fatal(err)
-	}
-	uiImage := ebiten.NewImageFromImage(img)
-
-	s, err := text.NewGoTextFaceSource(bytes.NewReader(goregular.TTF))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return &Context{
-		uiImage:      uiImage,
-		uiFaceSource: s,
-	}
-}
-
-type imageType int
-
-const (
-	imageTypeButton imageType = iota
-	imageTypeButtonPressed
-	imageTypeTextBox
-	imageTypeVScrollBarBack
-	imageTypeVScrollBarFront
-	imageTypeCheckBox
-	imageTypeCheckBoxPressed
-	imageTypeCheckBoxMark
-)
-
-var imageSrcRects = map[imageType]image.Rectangle{
-	imageTypeButton:          image.Rect(0, 0, 16, 16),
-	imageTypeButtonPressed:   image.Rect(16, 0, 32, 16),
-	imageTypeTextBox:         image.Rect(0, 16, 16, 32),
-	imageTypeVScrollBarBack:  image.Rect(16, 16, 24, 32),
-	imageTypeVScrollBarFront: image.Rect(24, 16, 32, 32),
-	imageTypeCheckBox:        image.Rect(0, 32, 16, 48),
-	imageTypeCheckBoxPressed: image.Rect(16, 32, 32, 48),
-	imageTypeCheckBoxMark:    image.Rect(32, 32, 48, 48),
-}
-
 const (
 	screenWidth  = 640
 	screenHeight = 480
@@ -89,59 +37,6 @@ const (
 
 type Input struct {
 	mouseButtonState int
-}
-
-func (c *Context) drawNinePatches(dst *ebiten.Image, dstRect image.Rectangle, srcRect image.Rectangle) {
-	srcX := srcRect.Min.X
-	srcY := srcRect.Min.Y
-	srcW := srcRect.Dx()
-	srcH := srcRect.Dy()
-
-	dstX := dstRect.Min.X
-	dstY := dstRect.Min.Y
-	dstW := dstRect.Dx()
-	dstH := dstRect.Dy()
-
-	op := &ebiten.DrawImageOptions{}
-	for j := 0; j < 3; j++ {
-		for i := 0; i < 3; i++ {
-			op.GeoM.Reset()
-
-			sx := srcX
-			sy := srcY
-			sw := srcW / 4
-			sh := srcH / 4
-			dx := 0
-			dy := 0
-			dw := sw
-			dh := sh
-			switch i {
-			case 1:
-				sx = srcX + srcW/4
-				sw = srcW / 2
-				dx = srcW / 4
-				dw = dstW - 2*srcW/4
-			case 2:
-				sx = srcX + 3*srcW/4
-				dx = dstW - srcW/4
-			}
-			switch j {
-			case 1:
-				sy = srcY + srcH/4
-				sh = srcH / 2
-				dy = srcH / 4
-				dh = dstH - 2*srcH/4
-			case 2:
-				sy = srcY + 3*srcH/4
-				dy = dstH - srcH/4
-			}
-
-			op.GeoM.Scale(float64(dw)/float64(sw), float64(dh)/float64(sh))
-			op.GeoM.Translate(float64(dx), float64(dy))
-			op.GeoM.Translate(float64(dstX), float64(dstY))
-			dst.DrawImage(c.uiImage.SubImage(image.Rect(sx, sy, sx+sw, sy+sh)).(*ebiten.Image), op)
-		}
-	}
 }
 
 type Button struct {
@@ -171,12 +66,12 @@ func (b *Button) Update() {
 	}
 }
 
-func (b *Button) Draw(dst *ebiten.Image, ctx *Context) {
+func (b *Button) Draw(dst *ebiten.Image, ctx *GameContext) {
 	t := imageTypeButton
 	if b.mouseDown {
 		t = imageTypeButtonPressed
 	}
-	ctx.drawNinePatches(dst, b.Rect, imageSrcRects[t])
+	ctx.drawNinePatches(dst, b.Rect, t)
 
 	op := &text.DrawOptions{}
 	op.GeoM.Translate(float64(b.Rect.Min.X+b.Rect.Max.X)/2, float64(b.Rect.Min.Y+b.Rect.Max.Y)/2)
@@ -273,12 +168,12 @@ func (v *VScrollBar) Update(contentHeight int) {
 	}
 }
 
-func (v *VScrollBar) Draw(dst *ebiten.Image, ctx *Context) {
+func (v *VScrollBar) Draw(dst *ebiten.Image, ctx *GameContext) {
 	sd := image.Rect(v.X, v.Y, v.X+VScrollBarWidth, v.Y+v.Height)
-	ctx.drawNinePatches(dst, sd, imageSrcRects[imageTypeVScrollBarBack])
+	ctx.drawNinePatches(dst, sd, imageTypeVScrollBarBack)
 
 	if v.thumbRate < 1 {
-		ctx.drawNinePatches(dst, v.thumbRect(), imageSrcRects[imageTypeVScrollBarFront])
+		ctx.drawNinePatches(dst, v.thumbRect(), imageTypeVScrollBarFront)
 	}
 }
 
@@ -332,8 +227,8 @@ func (t *TextBox) contentOffset() (int, int) {
 	return t.offsetX, t.offsetY
 }
 
-func (t *TextBox) Draw(dst *ebiten.Image, ctx *Context) {
-	ctx.drawNinePatches(dst, t.Rect, imageSrcRects[imageTypeTextBox])
+func (t *TextBox) Draw(dst *ebiten.Image, ctx *GameContext) {
+	ctx.drawNinePatches(dst, t.Rect, imageTypeTextBox)
 
 	textOp := &text.DrawOptions{}
 	x := -float64(t.offsetX) + textBoxPaddingLeft
@@ -367,7 +262,7 @@ type CheckBox struct {
 	onCheckChanged func(c *CheckBox)
 }
 
-func (c *CheckBox) width(ctx *Context) int {
+func (c *CheckBox) width(ctx *GameContext) int {
 	w := text.Advance(c.Text, &text.GoTextFace{
 		Source: ctx.uiFaceSource,
 		Size:   uiFontSize,
@@ -375,7 +270,7 @@ func (c *CheckBox) width(ctx *Context) int {
 	return checkboxWidth + checkboxPaddingLeft + int(w)
 }
 
-func (c *CheckBox) Update(ctx *Context) {
+func (c *CheckBox) Update(ctx *GameContext) {
 	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
 		x, y := ebiten.CursorPosition()
 		if c.X <= x && x < c.X+c.width(ctx) && c.Y <= y && y < c.Y+checkboxHeight {
@@ -394,15 +289,15 @@ func (c *CheckBox) Update(ctx *Context) {
 	}
 }
 
-func (c *CheckBox) Draw(dst *ebiten.Image, ctx *Context) {
+func (c *CheckBox) Draw(dst *ebiten.Image, ctx *GameContext) {
 	t := imageTypeCheckBox
 	if c.mouseDown {
 		t = imageTypeCheckBoxPressed
 	}
 	r := image.Rect(c.X, c.Y, c.X+checkboxWidth, c.Y+checkboxHeight)
-	ctx.drawNinePatches(dst, r, imageSrcRects[t])
+	ctx.drawNinePatches(dst, r, t)
 	if c.checked {
-		ctx.drawNinePatches(dst, r, imageSrcRects[imageTypeCheckBoxMark])
+		ctx.drawNinePatches(dst, r, imageTypeCheckBoxMark)
 	}
 
 	x := c.X + checkboxWidth + checkboxPaddingLeft
@@ -428,7 +323,7 @@ func (c *CheckBox) SetOnCheckChanged(f func(c *CheckBox)) {
 }
 
 type Game struct {
-	uiContext  *Context
+	uiContext  *GameContext
 	button1    *Button
 	button2    *Button
 	checkBox   *CheckBox
@@ -437,7 +332,7 @@ type Game struct {
 
 func NewGame() *Game {
 	g := &Game{
-		uiContext: NewContext(),
+		uiContext: NewGameContext(),
 	}
 	g.button1 = &Button{
 		Rect: image.Rect(16, 16, 144, 48),
